8f356ea1900e05463a27906f7c42d82a2946ab4f
Erion <avdiueri@gmail.com>
2023-08-20 21:06:42 +0200

    Removed console.logs

--- server/config/db.js
+++ server/config/db.js
@@ -5,7 +5,6 @@ const MONGODB_URI = process.env.MONGODB_URI;
 const connectDB = async () => {
     try{
         mongoose.set('strictQuery', false);
-        console.log(MONGODB_URI);
         const conn = await mongoose.connect(process.env.MONGODB_URI);
         console.log(`Db connected: ${conn.connection.host}`);  
       }catch(error){
--- server/controllers/users.controller.js
+++ server/controllers/users.controller.js
@@ -19,7 +19,6 @@ async function httpRegisterUser(req, res) {
 async function httpLogUserIn(req, res) {
     try{
         const { username, password } = req.body;
-        console.log(username, password);
         const token = await Login(username, password);
 
         res.cookie('token', token, {httpOnly: true});
