0c8596b20701f2846dbc311e38def4cd1d7e1ad4
Erion <avdiueri@gmail.com>
2023-08-22 12:23:50 +0200

    EmailSender small changes

--- server/helpers/email.sender.js
+++ server/helpers/email.sender.js
@@ -4,27 +4,28 @@ const smtpEmail = process.env.SMTP_EMAIL;
 const smtpEmailPass = process.env.SMTP_EMAIL_PASS;
 
 async function SendEmail(emailParameters) {
-    const transporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-            user: smtpEmail,
-            pass: smtpEmailPass
-        },
+    let transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        user: smtpEmail,
+        pass: smtpEmailPass
+      }
     });
 
-    const mailOptions = {
+    const message = {
         from: smtpEmail,
         to: smtpEmail,
         subject: 'Autobusi duhet të bëhet servis',
         text: `Autobusi ${emailParameters.vehicleModel} me regjistrim ${emailParameters.vehicleLicensePlates} duhet te behet servis.\nJane bere ${emailParameters.vehicleKilometres - emailParameters.vehicleServiceKilometres} km nga servisi i fundit!`
     };
 
-    try{
-        await transporter.sendMail(mailOptions);
-        transporter.close();
-    }catch(Error){
-        console.log(Error);
-    }
+    transporter.sendMail(message, function(err, data) {
+      if (err) {
+        console.log("Error " + err);
+      } else {
+        console.log("Email sent successfully");
+      }
+    });
 }
 
 module.exports = {
